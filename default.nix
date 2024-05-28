177ecc4e9751ec49d563f262f4b2b89cc85e1063
lavender hearts <hearts@colbatic.com>
2024-05-28 18:38:48 -0400

    feat: added nix build file

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ system ? builtins.currentSystem }:
+
+let
+  pkgs = import <nixpkgs> { inherit system; };
+in
+  with import <nixpkgs> {};
+	stdenv.mkDerivation rec {
+	  pname = "godotpcktool";
+	  version = "2.0";
+
+	  src = ./.;
+
+	  patches = [];
+
+	  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+	  enableParallelBuilding = true;
+
+	  nativeBuildInputs = [
+	  	clang
+		cmake
+	  ];
+
+	  installPhase = ''
+		install -D /build/GodotPckTool/build/src/godotpcktool $out/bin/godotpcktool
+	  '';
+
+	  meta = with lib; {
+		homepage = "https://github.com/gayhearts/godotpcktool";
+		description = "A standalone executable for unpacking and packing Godot .pck files.";
+		mainProgram = "godotpcktool";
+	  };
+	}
